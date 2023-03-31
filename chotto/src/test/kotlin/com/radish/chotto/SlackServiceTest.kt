f43aa0314978602b8f627aade8a909a3e8898c33
Radish <kouta.hara713@gmail.com>
2023-03-31 19:51:02 +0900

    wip

---
+++ chotto/src/test/kotlin/com/radish/chotto/SlackServiceTest.kt
@@ -0,0 +1,12 @@
+package com.radish.chotto
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SlackServiceTest {
+
+    @Test
+    fun postMessage() {
+    }
+}
